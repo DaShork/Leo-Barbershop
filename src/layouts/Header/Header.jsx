0c07296d1041a0afa87a63bc91bbce94d5d14e5e
DaShork <nhatminhvo2311@gmail.com>
2026-04-21 00:37:41 +0700

    feat(png path, config): update img filepath, update config vite and json for github-page deployment

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,9 +1,10 @@
+import logo from '../../assets/Leo_Barber.png'
 import './Header.css'
 
 function Header() {
     return (
         <header className = 'header'>
-            <img className ='header-logo' src = '/Leo_Barber.png' alt = 'Leo Barbershop' />
+            <img className ='header-logo' src = {logo} alt = 'Leo Barbershop' />
         </header>
     )
 }
