3822e50be6295f55a57611cb7d8327f86a25046d
DaShork <nhatminhvo2311@gmail.com>
2026-04-26 01:59:12 +0700

    feat(css+ui): add simple css for laying and outlining section

--- src/pages/LandingPage/components/HeroSection/HeroSection.jsx
+++ src/pages/LandingPage/components/HeroSection/HeroSection.jsx
@@ -4,20 +4,32 @@ import { getImage } from '/src/utils/getImage.js';
 import './HeroSection.css';
 
 
-const HeroSection = () => (
-  <Carousel autoplay>
-    <div>
-      <img src={getImage('/images/hero1.jpg')} alt='Leo Barbershop' className='hero-img' />
+function HeroSection() {
+  return (
+    <div className = 'hero-container'>
+      <Carousel autoplay>
+        <div>
+          <img src={getImage('/images/hero1.jpg')} alt='Leo Barbershop' className='hero-img' />
+        </div>
+        <div>
+          <img src={getImage('/images/hero2.jpg')} alt='Leo Barbershop' className='hero-img' />
+        </div>
+        <div>
+          <img src={getImage('/images/hero3.jpg')} alt='Leo Barbershop' className='hero-img' />
+        </div>
+        <div>
+          <img src={getImage('/images/hero4.jpg')} alt='Leo Barbershop' className='hero-img' />
+        </div>
+      </Carousel>
+      <div className='overlay'>
+        <h1 className = 'hero-title'>LEO BARBERSHOP</h1>
+        <p className = 'hero-sub'>EMBRACE YOUR UNIQUENESS</p>
+        <button className = 'hero-btn'>Đặt lịch ngay</button>
+      </div>
+
     </div>
-    <div>
-      <img src={getImage('/images/hero2.jpg')} alt='Leo Barbershop' className='hero-img' />
-    </div>
-    <div>
-      <img src={getImage('/images/hero3.jpg')} alt='Leo Barbershop' className='hero-img' />
-    </div>
-    <div>
-      <img src={getImage('/images/hero4.jpg')} alt='Leo Barbershop' className='hero-img' />
-    </div>
-  </Carousel>
-);
+  )
+
+}
+
 export default HeroSection
--- src/pages/LandingPage/components/Introduction/Introduction.jsx
+++ src/pages/LandingPage/components/Introduction/Introduction.jsx
@@ -1,12 +1,19 @@
 import { getImage } from '/src/utils/getImage.js';
+import './Introduction.css';
 
 function Introduction() {
     return (
         <div className = 'intro-container'>
-            <h2>Welcome to Our Barbershop</h2>
-            <p>We are a team of professional barbers dedicated to providing the best grooming experience for our clients.</p>
-            <img src={getImage('/images/model1.jpg')} alt='Leo Barbershop' className='intro-img'/>
-            <img src={getImage('/images/model2.jpg')} alt='Leo Barbershop' className='intro-img'/>
+            <div className = 'intro'>
+                <h2>Chào mừng đên Leo Barber</h2>
+                <h1>GIỚI THIỆU</h1>
+                <p>Chào mừng bạn đến với Leo Barbershop, nơi không chỉ đơn thuần là cắt tóc,
+                mà là nơi chúng tôi kiến tạo nên phong cách và sự tự tin cho phái mạnh.</p>
+            </div>
+            <div className = 'intro-img'>
+                <img src={getImage('/images/model1.jpg')} alt='Leo Barbershop' className='intro-img'/>
+                <img src={getImage('/images/model2.jpg')} alt='Leo Barbershop' className='intro-img'/>
+            </div>       
         </div>
     )
 }
